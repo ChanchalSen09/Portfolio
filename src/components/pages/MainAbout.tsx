e167b23ff314557f16e451ba99726dee9da4fe28
Chanchal Sen <92750541+ChanchalSen09@users.noreply.github.com>
2025-02-24 18:33:40 +0000

    --

--- src/components/pages/MainAbout.tsx
+++ src/components/pages/MainAbout.tsx
@@ -3,14 +3,14 @@ import { TextFade, TypingEffect } from "../animations/TextAnimations";
 import AvatarImg from "../../assets/Avatar.webp";
 function MainAbout() {
   return (
-    <div className=" flex flex-col md:flex-row items-center  justify-center text-center md:text-left">
+    <div className="flex flex-col items-center justify-center text-center  md:flex-row md:text-left">
       <div className="">
         <TextFade direction="up">
-          <h1 className="text-5xl md:text-6xl font-bold text-black">
+          <h1 className="text-5xl font-bold text-black md:text-6xl">
             Chanchal Sen
           </h1>
         </TextFade>
-        <p className="mt-6 text-lg md:text-xl text-gray-600 leading-relaxed ">
+        <p className="mt-6 text-lg leading-relaxed text-gray-600 md:text-xl ">
           <TextFade direction="up">
             <p className="font-bold">
               MERN Stack Developer | React.js Specialist | AI/ML Enthusiast
@@ -28,11 +28,12 @@ function MainAbout() {
         initial={{ opacity: 0, x: 50 }}
         animate={{ opacity: 1, x: 0 }}
         transition={{ duration: 0.8 }}
-        className="w-full md:w-2/5 flex justify-center mt-6 md:mt-0">
+        className="flex justify-center w-full mt-6 md:w-2/5 md:mt-0">
         <img
           src={AvatarImg}
           alt="Chanchal Sen"
-          className="w-56 h-56 md:w-72 md:h-72 rounded-full shadow-lg object-cover border-2 border-gray-300"
+          loading="lazy"
+          className="object-cover w-56 h-56 border-2 border-gray-300 rounded-full shadow-lg md:w-72 md:h-72"
         />
       </motion.div>
     </div>
